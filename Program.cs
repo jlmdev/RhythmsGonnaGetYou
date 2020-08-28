06b60a49eb8f02d6e7133af736936fd901b0204d
Josh Mann <jlmann72@gmail.com>
2020-08-28 10:43:49 -0400

    Working on menu creation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    // Create Band class to represent Bands table in database
+    class Band
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public int NumberOfMembers { get; set; }
+        public string Website { get; set; }
+        public string Style { get; set; }
+        public bool IsSigned { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhoneNumber { get; set; }
     }
-  }
+
+    // Create Album class to represent Albums table in database
+    class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsExplicit { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int BandId { get; set; }
+        public Band Band { get; set; }
+    }
+
+    // Create context to connect to database
+    class RhythmTestContext : DbContext
+    {
+        public DbSet<Band> Bands { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("server=localhost;database=RhythmsGonnaGetYou");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the connection context and references to the tables
+            var context = new RhythmTestContext();
+
+            var bands = context.Bands;
+            var albums = context.Albums.Include(album => album.BandId);
+
+            // Create While Loop for Program
+            var runProgram = true;
+            while (runProgram)
+            {
+                // TODO: Show Menu
+                // TODO: Create Menu Options
+                //      TODO: Option: Add a new band (ab)
+                //      TODO: Option: View all bands (vb)
+                //      TODO: Option: Add an album for a band (aa)
+                //      TODO: Option: Unsign a band (u)
+                //      TODO: Option: Re-sign a band (r)
+
+
+
+
+
+
+            }
+        }
+
+    }
 }
